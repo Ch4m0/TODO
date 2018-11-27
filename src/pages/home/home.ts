28f32a7eccbcbb2f88f4524eb7bf9cc50c471575
Ivan Verano <javier.vasquez@mas57.co>
2018-11-27 11:10:33 -0500

    add pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,52 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	public tareas:any;
 
   constructor(public navCtrl: NavController) {
+
+  	this.tareas = [
+  	{
+  		id: 1,
+  		titulo: 'Tarea1',
+  		descripcion:'Esto descripcion',
+  		categoria:'categoria1',
+  		fecha: '12/01/2019',
+  		estado: 1
+  	},
+  	{
+  		id: 2,
+  		titulo: 'Tarea2',
+  		descripcion:'Esto descripcion',
+  		categoria:'categoria1',
+  		fecha: '12/01/2019',
+  		estado: 0
+  	},
+  	{
+  		id:3,
+  		titulo: 'Tarea3',
+  		descripcion:'Esto descripcion',
+  		categoria:'categoria1',
+  		fecha: '12/01/2019',
+  		estado: 1
+
+  	},
+  	{
+  		id: 4,
+  		titulo: 'Tarea4',
+  		descripcion:'Esto descripcion',
+  		categoria:'categoria1',
+  		fecha: '12/01/2019',
+  		estado: 1
+
+  	}
+
+  	];
+
+  	console.log(this.tareas)
+  	console.log(this.tareas)
+  	console.log(this.tareas)
+  	console.log(this.tareas)
 
   }
 
